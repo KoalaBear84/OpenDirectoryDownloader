e19de4fb838051eb7f5028df590b84afaa91b0da
KoalaBear <animatrix84@gmail.com>
2020-04-01 07:01:04 +0200

    - Fix for failed test

--- OpenDirectoryDownloader/DirectoryParser.cs
+++ OpenDirectoryDownloader/DirectoryParser.cs
@@ -1859,7 +1859,7 @@ namespace OpenDirectoryDownloader
                 link.TextContent.ToLower() != "parent directory" &&
                 link.TextContent.Trim() != "Name" &&
                 linkHref?.Contains("&expand") == false &&
-                !new Regex(@"\?[NMSD][AD]").IsMatch(linkHref) &&
+                (!new Regex(@"\?[NMSD][AD]").IsMatch(linkHref) || linkHref.StartsWith("DirectoryList.asp")) &&
                 (Path.GetFileName(linkHref) != "DirectoryList.asp" || !string.IsNullOrWhiteSpace(link.TextContent));
         }
     }
