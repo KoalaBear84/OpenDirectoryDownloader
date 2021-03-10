ec9773da4e910dd1d0443d0a54fd99ac21971396
KoalaBear <animatrix84@gmail.com>
2021-03-10 07:34:18 +0100

    - Fixed ufile.io fallback

--- OpenDirectoryDownloader/FileUpload/UploadFilesIo.cs
+++ OpenDirectoryDownloader/FileUpload/UploadFilesIo.cs
@@ -1,8 +1,12 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NLog;
+using OpenDirectoryDownloader.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace OpenDirectoryDownloader.FileUpload
@@ -13,54 +17,87 @@ namespace OpenDirectoryDownloader.FileUpload
 
         public static async Task<UploadFilesIoFile> UploadFile(HttpClient httpClient, string path)
         {
-            using (MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent($"Upload----{Guid.NewGuid()}"))
+            int retries = 0;
+            int maxRetries = 5;
+
+            while (retries < maxRetries)
             {
-                multipartFormDataContent.Add(new StreamContent(new FileStream(path, FileMode.Open)), "file", Path.GetFileName(path));
-
-                int i = 0;
-                int retries = 5;
-
-                while (i < retries)
+                try
                 {
-                    try
+                    string html = await httpClient.GetStringAsync("https://ufile.io");
+                    Match regexMatchCSRFToken = new Regex("name=\"csrf_test_name\" value=\"(?<CSRFToken>.*)\"").Match(html);
+                    Match regexMatchSessionId = new Regex("<input.*?id=\"session_id\".*?value=\"(?<SessionId>.*)\"").Match(html);
+
+                    if (regexMatchCSRFToken.Success && regexMatchSessionId.Success)
                     {
-                        using (HttpResponseMessage httpResponseMessage = await httpClient.PostAsync("https://up.uploadfiles.io/upload", multipartFormDataContent))
+                        string csrfToken = regexMatchCSRFToken.Groups["CSRFToken"].Value;
+                        string sessionId = regexMatchSessionId.Groups["SessionId"].Value;
+
+                        // Create session
+                        Dictionary<string, string> postValuesCreateSession = new Dictionary<string, string>
                         {
-                            if (httpResponseMessage.IsSuccessStatusCode)
-                            {
-                                string response = await httpResponseMessage.Content.ReadAsStringAsync();
+                            { "csrf_test_name", csrfToken },
+                            { "file_size", new FileInfo(path).Length.ToString() }
+                        };
 
-                                Logger.Debug($"Response from Uploadfiles.io: {response}");
+                        HttpRequestMessage httpRequestMessageCreateSession = new HttpRequestMessage(HttpMethod.Post, "https://up.ufile.io/v1/upload/create_session") { Content = new FormUrlEncodedContent(postValuesCreateSession) };
+                        HttpResponseMessage httpResponseMessageCreateSession = await httpClient.SendAsync(httpRequestMessageCreateSession);
 
-                                return JsonConvert.DeserializeObject<UploadFilesIoFile>(response);
-                            }
-                            else
+                        JObject resultCreateSession = JObject.Parse(await httpResponseMessageCreateSession.Content.ReadAsStringAsync());
+
+                        string fileId = resultCreateSession["fuid"].Value<string>();
+
+                        if (string.IsNullOrEmpty(fileId))
+                        {
+                            throw new Exception("Ufile.io error, error creating session");
+                        }
+
+                        // Post file
+                        using (MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent($"Upload----{Guid.NewGuid()}"))
+                        {
+                            multipartFormDataContent.Add(new StringContent("1"), "chunk_index");
+                            multipartFormDataContent.Add(new StringContent(fileId), "fuid");
+                            multipartFormDataContent.Add(new StreamContent(new FileStream(path, FileMode.Open)), "file", Path.GetFileName(path));
+
+                            using (HttpResponseMessage httpResponseMessage = await httpClient.PostAsync("https://up.ufile.io/v1/upload/chunk", multipartFormDataContent))
                             {
-                                retries++;
-                                Logger.Error($"Error uploading file... Retry in 5 seconds!!!");
-                                await Task.Delay(TimeSpan.FromSeconds(5));
+                                httpResponseMessage.EnsureSuccessStatusCode();
                             }
                         }
-                    }
-                    catch (Exception)
-                    {
-                        Logger.Error($"Error uploading file... Retry in 5 seconds!!!");
-                        await Task.Delay(TimeSpan.FromSeconds(5));
+
+                        // Finalise
+                        Dictionary<string, string> postValuesFinalise = new Dictionary<string, string>
+                        {
+                            { "csrf_test_name", csrfToken },
+                            { "fuid", fileId },
+                            { "file_name", Path.GetFileName(path) },
+                            { "file_type", Path.GetExtension(path) },
+                            { "total_chunks", "1" },
+                            { "session_id", sessionId },
+                        };
+
+                        HttpRequestMessage httpRequestMessageFinalise = new HttpRequestMessage(HttpMethod.Post, "https://up.ufile.io/v1/upload/finalise") { Content = new FormUrlEncodedContent(postValuesFinalise) };
+                        HttpResponseMessage httpResponseMessageFinalise = await httpClient.SendAsync(httpRequestMessageFinalise);
+
+                        return JsonConvert.DeserializeObject<UploadFilesIoFile>(await httpResponseMessageFinalise.Content.ReadAsStringAsync());
                     }
 
                     retries++;
                 }
-
-                throw new Exception("Error uploading Urls file...");
+                catch (Exception)
+                {
+                    retries++;
+                    Logger.Error($"Error uploading file... Retry in 5 seconds!!!");
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                }
             }
+
+            throw new FriendlyException("Error uploading URLs");
         }
     }
 
     public class UploadFilesIoFile
     {
-        [JsonProperty("status")]
-        public bool Status { get; set; }
-
         [JsonProperty("id")]
         public long Id { get; set; }
 
@@ -88,10 +125,8 @@ namespace OpenDirectoryDownloader.FileUpload
         [JsonProperty("expiry")]
         public string Expiry { get; set; }
 
-        [JsonProperty("session_id")]
-        public string SessionId { get; set; }
-
-        [JsonProperty("timing")]
-        public string Timing { get; set; }
+        [JsonProperty("location")]
+        public string Location { get; set; }
     }
+
 }
