fb35aca5225693c8f1f30ce9f2ba5c85414e4537
KoalaBear <animatrix84@gmail.com>
2021-10-25 08:02:49 +0200

    - Update Chrome UserAgent

--- src/OpenDirectoryDownloader/Constants.cs
+++ src/OpenDirectoryDownloader/Constants.cs
@@ -17,7 +17,7 @@
         public class UserAgent
         {
             public const string Curl = "curl/7.55.1";
-            public const string Chrome = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3800.0 Safari/537.36";
+            public const string Chrome = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.1043.1 Safari/537.36";
         }
 
         public class UriScheme
