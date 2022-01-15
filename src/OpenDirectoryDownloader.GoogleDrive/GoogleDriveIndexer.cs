5f39e9856061666d7ac51f3ee2473e8c51c41443
KoalaBear <animatrix84@gmail.com>
2022-01-16 00:14:13 +0100

    - Google Drive retries

--- src/OpenDirectoryDownloader.GoogleDrive/GoogleDriveIndexer.cs
+++ src/OpenDirectoryDownloader.GoogleDrive/GoogleDriveIndexer.cs
@@ -69,98 +69,117 @@ public static class GoogleDriveIndexer
 		string nextPageToken = string.Empty;
 		string folderId = webDirectory.Uri.Segments.Last();
 
-		do
+		bool rateLimitException = false;
+
+		int retries = 0;
+		int maxRetries = 5;
+
+		while (retries < maxRetries || rateLimitException || !string.IsNullOrWhiteSpace(nextPageToken))
 		{
-			bool rateLimitException = false;
-
-			do
+			try
 			{
-				try
+				await RateLimiter.RateLimit();
+
+				Logger.Debug($"Started Google Drive Request for Folder {folderId}");
+
+				if (!string.IsNullOrWhiteSpace(resourceKey))
 				{
-					await RateLimiter.RateLimit();
-
-					Logger.Debug($"Started Google Drive Request for Folder {folderId}");
-
-					if (!string.IsNullOrWhiteSpace(resourceKey))
+					DriveService.HttpClient.DefaultRequestHeaders.Add("X-Goog-Drive-Resource-Keys", $"{folderId}/{resourceKey}");
+				}
+				else
+				{
+					if (DriveService.HttpClient.DefaultRequestHeaders.Contains("X-Goog-Drive-Resource-Keys"))
 					{
-						DriveService.HttpClient.DefaultRequestHeaders.Add("X-Goog-Drive-Resource-Keys", $"{folderId}/{resourceKey}");
+						DriveService.HttpClient.DefaultRequestHeaders.Remove("X-Goog-Drive-Resource-Keys");
+					}
+				}
+
+				FilesResource.ListRequest listRequest = DriveService.Files.List();
+				listRequest.PageSize = 1000;
+				listRequest.Q = $"'{folderId}' in parents";
+				listRequest.PageToken = nextPageToken;
+				listRequest.Fields = "nextPageToken, files(id, name, mimeType, size, shortcutDetails, resourceKey)";
+				listRequest.IncludeItemsFromAllDrives = true;
+				listRequest.SupportsAllDrives = true;
+				Google.Apis.Drive.v3.Data.FileList fileList = await listRequest.ExecuteAsync();
+
+				foreach (Google.Apis.Drive.v3.Data.File file in fileList.Files.OrderByDescending(f => f.MimeType == FolderMimeType || f.MimeType == ShortcutMimeType).ThenBy(f => f.Name))
+				{
+					string mimeType = file.ShortcutDetails?.TargetMimeType ?? file.MimeType;
+					string id = file.ShortcutDetails?.TargetId ?? file.Id;
+
+					bool isFile = mimeType != FolderMimeType;
+
+					if (!isFile)
+					{
+						string url = $"https://drive.google.com/drive/folders/{id}";
+
+						if (!string.IsNullOrEmpty(file.ResourceKey))
+						{
+							url += $"?resourcekey={file.ResourceKey}";
+						}
+
+						webDirectory.Subdirectories.Add(new WebDirectory(webDirectory)
+						{
+							Url = url,
+							Name = file.Name
+						});
 					}
 					else
 					{
-						if (DriveService.HttpClient.DefaultRequestHeaders.Contains("X-Goog-Drive-Resource-Keys"))
+						string url = $"https://drive.google.com/uc?export=download&id={id}";
+
+						if (!string.IsNullOrEmpty(file.ResourceKey))
 						{
-							DriveService.HttpClient.DefaultRequestHeaders.Remove("X-Goog-Drive-Resource-Keys");
+							url += $"&resourcekey={file.ResourceKey}";
 						}
+
+						webDirectory.Files.Add(new WebFile
+						{
+							Url = url,
+							FileName = file.Name,
+							FileSize = file.Size ?? 0
+						});
 					}
-
-					FilesResource.ListRequest listRequest = DriveService.Files.List();
-					listRequest.PageSize = 1000;
-					listRequest.Q = $"'{folderId}' in parents";
-					listRequest.PageToken = nextPageToken;
-					listRequest.Fields = "nextPageToken, files(id, name, mimeType, size, shortcutDetails, resourceKey)";
-					listRequest.IncludeItemsFromAllDrives = true;
-					listRequest.SupportsAllDrives = true;
-					Google.Apis.Drive.v3.Data.FileList fileList = await listRequest.ExecuteAsync();
-
-					foreach (Google.Apis.Drive.v3.Data.File file in fileList.Files.OrderByDescending(f => f.MimeType == FolderMimeType || f.MimeType == ShortcutMimeType).ThenBy(f => f.Name))
-					{
-						string mimeType = file.ShortcutDetails?.TargetMimeType ?? file.MimeType;
-						string id = file.ShortcutDetails?.TargetId ?? file.Id;
-
-						bool isFile = mimeType != FolderMimeType;
-
-						if (!isFile)
-						{
-							string url = $"https://drive.google.com/drive/folders/{id}";
-
-							if (!string.IsNullOrEmpty(file.ResourceKey))
-							{
-								url += $"?resourcekey={file.ResourceKey}";
-							}
-
-							webDirectory.Subdirectories.Add(new WebDirectory(webDirectory)
-							{
-								Url = url,
-								Name = file.Name
-							});
-						}
-						else
-						{
-							string url = $"https://drive.google.com/uc?export=download&id={id}";
-
-							if (!string.IsNullOrEmpty(file.ResourceKey))
-							{
-								url += $"&resourcekey={file.ResourceKey}";
-							}
-
-							webDirectory.Files.Add(new WebFile
-							{
-								Url = url,
-								FileName = file.Name,
-								FileSize = file.Size ?? 0
-							});
-						}
-					}
-
-					nextPageToken = fileList.NextPageToken;
-
-					rateLimitException = false;
 				}
-				catch (GoogleApiException ex)
+
+				nextPageToken = fileList.NextPageToken;
+
+				rateLimitException = false;
+
+				if (retries > 0)
 				{
-					rateLimitException = ex.Error.Message == "User rate limit exceeded.";
-
-					if (rateLimitException)
-					{
-						Logger.Debug($"Google Drive rate limit, try again");
-					}
-					else
-					{
-						Logger.Error($"Google Drive error: {ex}");
-					}
+					Logger.Warn($"Retrieval succesful after try {retries + 1} for {webDirectory.Url}");
 				}
-			} while (rateLimitException);
-		} while (!string.IsNullOrWhiteSpace(nextPageToken));
+
+				if (string.IsNullOrWhiteSpace(nextPageToken))
+				{
+					break;
+				}
+
+				retries = 0;
+			}
+			catch (GoogleApiException ex)
+			{
+				rateLimitException = ex.Error?.Message == "User rate limit exceeded.";
+
+				if (rateLimitException)
+				{
+					Logger.Debug($"Google Drive rate limit, try again");
+				}
+				else
+				{
+					retries++;
+					Logger.Warn($"Google Drive error for {webDirectory.Url} on try {retries + 1}: {ex.Message}");
+				}
+
+				if (retries == maxRetries)
+				{
+					Logger.Error($"Skip {webDirectory.Url} because of {maxRetries} consecutive errors on : {ex.Message}");
+					return webDirectory;
+				}
+			}
+		}
 
 		Logger.Debug($"Finished Google Drive Request for Folder {folderId}");
 
